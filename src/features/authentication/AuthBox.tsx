3749fdd660d6b8e719c1573ca7b5cbad7eedf1ac
Tomo <tomobratic23@gmail.com>
2025-08-07 11:54:22 +0200

    Render AuthBox content based on URL (/signup or /login)

--- src/features/authentication/AuthBox.tsx
+++ src/features/authentication/AuthBox.tsx
@@ -1,17 +1,30 @@
+import { Link, useLocation } from "react-router";
 import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 
 function AuthBox() {
+  const { pathname } = useLocation();
+
+  const isSignup = pathname === "/signup";
+
   return (
     <div className="mb-36 grid place-items-center pt-8">
       <div className="rounded-lg bg-black/70 px-32 py-40">
-        <h1 className="text-center text-3xl font-semibold">Sign in</h1>
+        <h1 className="text-center text-3xl font-semibold">
+          {isSignup ? "Sign up" : "Login"}
+        </h1>
         <p className="pt-1 pb-3 text-center">
-          Don't have an account?
-          <span className="font-medium text-red-600 underline">Sign up</span>
+          Do {!isSignup && "not"} have an account?
+          <Link
+            className="font-medium text-red-600 underline"
+            to={`${isSignup ? "/login" : "/signup"}`}
+          >
+            {isSignup ? "Login" : "SignUp"}
+          </Link>
         </p>
 
         {/* LOGIN/SIGN UP FORM */}
-        <LoginForm />
+        {isSignup ? <SignupForm /> : <LoginForm />}
       </div>
     </div>
   );
