f067e4cc868ba842af98bf88a3e84c0fecdcbe38
Tomo <tomobratic23@gmail.com>
2025-08-13 13:29:36 +0200

    Setup React-Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "./routes/AppRoutes";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
