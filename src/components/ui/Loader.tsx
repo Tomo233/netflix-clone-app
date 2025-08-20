a4b263dfe49008f21ebc4c3640b9b9b87821acc4
Tomo <tomobratic23@gmail.com>
2025-08-20 12:19:42 +0200

    Add mui linear progress component to Loader custom component

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,7 +1,12 @@
+import { Box, LinearProgress } from "@mui/material";
+
 function Loader() {
   return (
     <div className="pt-10">
       <div className="mb-5 h-9 w-72 bg-[#63626311]" />
+      <Box sx={{ width: "58rem", marginY: "15px" }}>
+        <LinearProgress color="inherit" />
+      </Box>
       <div className="flex gap-5">
         <div className="h-36 w-72 bg-[#63626311]" />
         <div className="h-36 w-72 bg-[#63626311]" />
