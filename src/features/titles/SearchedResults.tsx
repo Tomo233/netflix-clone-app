466c4f5546132524be9da65bc539654dc4505716
Tomo <tomobratic23@gmail.com>
2025-08-28 14:57:02 +0200

    Create moreToExplore derived variable to display titles similar to search

--- src/features/titles/SearchedResults.tsx
+++ src/features/titles/SearchedResults.tsx
@@ -6,8 +6,6 @@ import TitleCard from "./TitleCard";
 function SearchedResults() {
   const { searchedResults, isLoadingSearchedResults } = useSearchedResults();
 
-  console.log(searchedResults);
-
   if (isLoadingSearchedResults) return <SkeletonLoader />;
 
   if (searchedResults?.length === 0)
@@ -17,24 +15,25 @@ function SearchedResults() {
       </p>
     );
 
+  const moreToExplore = searchedResults
+    ?.slice(0, 5)
+    .map((result) => result.titleName);
+
   return (
-    <>
-      <div className="my-10 mb-10 flex items-center gap-5">
+    <div>
+      <div className="my-12 flex items-center gap-5">
         <span className="text-secondary-text-color text-lg tracking-wide">
           More to explore:
         </span>
-        <ul className="flex cursor-pointer gap-3 font-medium tracking-wide">
-          <li className="transition-colors duration-300 ease-in-out hover:text-red-500">
-            Breaking
-          </li>
-          <span>|</span>
-          <li>Breaking Bad</li>
-          <span>|</span>
-          <li>Breaking Down</li>
-          <span>|</span>
-          <li>Breaking In</li>
-          <span>|</span>
-          <li>Prison Break</li>
+        <ul className="flex cursor-pointer gap-5 font-medium tracking-wide">
+          {moreToExplore?.map((item) => (
+            <>
+              <li className="transition-colors duration-300 ease-in-out hover:text-red-500">
+                {item}
+              </li>
+              <span>|</span>
+            </>
+          ))}
         </ul>
       </div>
       <Row>
@@ -42,7 +41,7 @@ function SearchedResults() {
           <TitleCard title={result} key={result.id} />
         ))}
       </Row>
-    </>
+    </div>
   );
 }
 
